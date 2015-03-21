fe27ef426cb94fabacf760389444c829a0dcebc6
Alex Milanov <alex@i4web.biz>
2015-03-21 20:33:50 +0200

    express jade posts logic

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // dependancies
 var express = require('express')
 var fs = require("fs");
+var posts = require("./posts.json");
 
 // create app
 var app = express()
@@ -15,7 +16,7 @@ app.set('view engine', 'jade');
 
 // routes
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {posts: posts});
 })
 
 // listen for files: /post -> /views/post.jade
@@ -23,8 +24,15 @@ app.get("/:fileName", function(req, res, next){
   if(req.params && req.params.fileName){
     var fileName = req.params.fileName.replace(".html","");
 
+    // if jade file exists
     if(fs.existsSync(__dirname+"/views/"+fileName+".jade")){
-        res.render(fileName);
+      res.render(fileName);
+    // if post is in posts
+    } else if (posts[fileName]) {
+      res.render("post", {post: posts[fileName]});
+    // else continue
+    } else {
+      next();
     }
 
   } else {
